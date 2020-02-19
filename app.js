e9a831b13b11919cf83829e44061b6603584cbde
kowshik571 <61055082+kowshik571@users.noreply.github.com>
2020-02-19 12:28:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,40 +2,90 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(a,b,c)
+{
+	var address;
+	var distanceFromTown;
+	var hasNeighbours;
+}
+return a+b+c;
 
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
-
+var count ,c;
+if(noofSiblings>0)
+{
+	count++;
+	nasNeighbours--;
+}
+else
+{
+	flag=0;
+}
+if(flag!=0)
+{
+	console.log(count)
+}
+else{
+	console.log(there are no neighbours)
+}
+if(c=NuclearFamily)
+{
+	consloe.log(true)
+}
+else{
+	consloe.load(false)
+}
 //Progression 3:
 //Lily is suspicious about Karen's new friend
+lily>=karen's new friend;
 //Karen tells her friend's age and even writes it down
+consloe.log(karen.age)
 //Check which one those is not a number (NaN) and return that value
-
+var a=NaN;
 //Progression 4:
 //Lily gave Karen x sweets
+karen=x sweets
 //Karen ate y sweets herself
+ y.sweets=karen
 //On her way to the river, she ate another z sweets every n meters travelled
-//Her friend divided the remaining sweets into 2 parts for each
-//How many sweets did her friend get to eat?
+she.z=sweets;
+she-n meters;
 
+//Her friend divided the remaining sweets into 2 parts for each
+she-n/2
+//How many sweets did her friend get to eat?
+she-n
 //Progression 5:
 //As Lily moves closer, it gets colder. She checks the temperature on her mobile
+lil==colder
 //It only shows in fahrenheit. Convert the data to celsius and return it.
-
+fahrenheit=>celsius
 //Progression 6:
 //Lily can now do multiple things to deal with this
+lily*deal
 //1. Take her daughter to a doctor
+lili<=doctor
 //2. Talk to her husband about it
+doctor=>husband
 //3. Counsel her daughter herself
+daughter<=council
 //4. Lock her daughter in her room
+daughter.lock
 //Given a value, return which of these above actions Lily would take
-
+lily*deal
 //Progression 7:
 //Lily realized that she'd hurt her daughter
+lily!=daughter
 //All she wants now is for her to stop crying
+all=crying
 //She refuses to talk to her but Lily doesn't stop trying
+talk=doesn't stop crying
 //She tries out multiple things hoping for the best
+she.best
 //Take all of Lily's strategies and concatenate them to a single var
+lily+single
 //Seperate the strategies by a single space
+
 //Return the length of the complete strategy
